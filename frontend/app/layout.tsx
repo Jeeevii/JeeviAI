8adc648055a44e227fc3e68dab1f9aadc2c6696b
Jeevi <JeevithanMahenthran@gmail.com>
2025-07-14 16:53:24 -0700

    updated metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,58 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Analytics } from '@vercel/analytics/next';
+import React from 'react'
 
+import { Analytics } from '@vercel/analytics/next';
 export const metadata: Metadata = {
-  title: 'Jeevithan Mahenthran | Portfolio',
-  description: 'Jeevithan Mahenthran Full Stack Developer Portfolio',
+  title: 'Jeevithan Mahenthran | UCSC CS & Game Design Portfolio',
+  description: 'hi! I\'m Jeevithan Mahenthran, a CS student at UC Santa Cruz. This is my personal portfolio where i share my projects ive built, companies and skills ive learned, and a bit about me!',
+  keywords: [
+    'Jeevithan Mahenthran',
+    'Jeevi',
+    'Portfolio',
+    'UCSC',
+    'University of California Santa Cruz',
+    'Computer Science',
+    'Game Design',
+    'Web Development',
+    'Backend Developer',
+    'Full Stack Developer',
+    'Software Engineering Intern',
+    'Student Developer', 
+    'AI Developer',
+    'Projects',
+    'Resume',
+    'GitHub',
+    'LinkedIn',
+    'AI Projects',
+    'Personal Website',
+    'Tech Blog',
+    'Programming',
+    'Software Engineer',
+    'Developer Portfolio'
+  ],
+  authors: [
+    { name: 'Jeevithan Mahenthran', url: 'https://jeevithanmahenthran.com' }
+  ],
+  creator:
+    'Jeevithan Mahenthran',
+  openGraph: {
+    title: 'Jeevithan Mahenthran | UCSC CS & Game Design Portfolio',
+    description: 'hi! I\'m Jeevithan Mahenthran, a CS student at UC Santa Cruz. This is my personal portfolio where i share the projects ive built, companies and skills ive learned, and a bit about me!',
+    url: 'https://jeevithanmahenthran.com',
+    siteName: 'Jeevithan Mahenthran Portfolio',
+    images: [
+      {
+        url: 'https://jeevithanmahenthran.com/jeevi.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Jeevithan Mahenthran Portfolio',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  metadataBase: new URL('https://jeevithanmahenthran.com'),
 }
 
 export default function RootLayout({
